484c928e3d438c7d382da1349b154ff25c9d4651
emersonoliveira <eoliveirabra@gmail.com>
2022-02-04 11:53:29 -0300

    11:53 AM, 2/4/2022

--- Project3/Source.cpp
+++ Project3/Source.cpp
@@ -12,6 +12,10 @@ This program displays a message
 
 int main()
 {
+	// DECLARATION STATEMENTS
+
+	// OTHERS STATEMENTS
+
 	cout << "\n";												// This is a comment
 	cout << "\n";												// This is a comment
 	cout << "\n";												// This is a comment
@@ -65,5 +69,69 @@ int main()
 	cout << "Practice: " << (20 - 2) / (6 + 3)	<< endl;
 	cout << "Practice: " << 50 % 20				<< endl;
 	cout << "Practice: " << (10 + 3) % 4		<< endl;
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+
+	int amount = 1;
+	int m = 50;
+	int n = 10;
+	int p = 5;
+
+	cout << "Practice: " << n / p + 3 << endl;
+	cout << "Practice: " << m / p + n - 10 * amount << endl;
+	cout << "Practice: " << m - 3 * n + 4 * amount << endl;
+	cout << "Practice: " << amount / 5 << endl;
+	cout << "Practice: " << 18 / p << endl;
+	cout << "Practice: " << -p * n << endl;
+	cout << "Practice: " << -p / 20 << endl;
+	cout << "Practice: " << (m + n) / (p + amount) << endl;
+	cout << "Practice: " << m + n / p + amount << endl;
+
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+
+
+	double amount2 = 1.00;		// declaration statement: dataType variableName
+	double m2 = 50.00;			// declaration statement: dataType variableName
+	double n2 = 10.00;			// declaration statement: dataType variableName
+	double p2 = 5.00;			// declaration statement: dataType variableName
+
+	cout << "Practice: " << n2 / p2 + 3 << endl;
+	cout << "Practice: " << m2 / p2 + n2 - 10 * amount2 << endl;
+	cout << "Practice: " << m2 - 3 * n2 + 4 * amount2 << endl;
+	cout << "Practice: " << amount2 / 5 << endl;
+	cout << "Practice: " << 18 / p2 << endl;
+	cout << "Practice: " << -p2 * n2 << endl;
+	cout << "Practice: " << -p2 / 20 << endl;
+	cout << "Practice: " << (m2 + n2) / (p2 + amount2) << endl;
+	cout << "Practice: " << m2 + n2 / p2 + amount2 << endl;
+
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "PROGRAM 2.7\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+
+	// Declaration statements
+
+	double grade1;		// declare grade1 as double variable
+	double grade2;		// declare grade2 as double variable
+	double total;		// declare total as double variable
+	double average;		// declare average as double variable
+
+
+	grade1 = 85.5;
+	grade2 = 97.0;
+	total = grade1 + grade2;
+	average = total / 2.0;		// divide the total by 2.0
+	cout << "The average grade is " << average << endl;
 	return 0;
 }
